43dd7a32fc74dbf1412d300f13be5994daf744ee
BuildTools <n.novikov@cargoonline.ru>
2020-12-18 00:29:04 +0300

    fix errors

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,12 +6,9 @@ export const getRules = () => 'Answer "yes" if given number is prime. Otherwise 
 const isValid = (input) => ['yes', 'no'].includes(input)
 
 const isPrime = (number) => {
-  for (let i = 2; i <= number / 2; i += 1) {
-    if (number % i === 0) {
-      return false
-    }
-  }
-  return true
+  for(let i = 2; i < number; i += 1)
+    if (number % i === 0) return false;
+  return number > 1;
 }
 
 export const run = () => {
