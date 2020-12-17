5f8ebdd1b7c674acf23ea0f7f9440f49195c2088
BuildTools <n.novikov@cargoonline.ru>
2020-12-18 00:23:42 +0300

    fix errors

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,28 +1,28 @@
-import { showQuestion, setUserInput } from '../../utils/interaction.js';
-import { getRandomInt, isInt } from '../../utils/utils.js';
+import { showQuestion, setUserInput } from '../../utils/interaction.js'
+import { getRandomInt, isInt } from '../../utils/utils.js'
 
-export const getRules = () => 'Find the greatest common divisor of given numbers.';
+export const getRules = () => 'Find the greatest common divisor of given numbers.'
 
 const gcd = (num1, num2) => {
   if (num2) {
-    return gcd(num2, num1 % num2);
+    return gcd(num2, num1 % num2)
   }
 
-  return Math.abs(num1);
-};
+  return Math.abs(num1)
+}
 
 export const run = () => {
-  let resultGame = true;
-  const [number1, number2] = [getRandomInt(50), getRandomInt(50)];
+  let resultGame = true
+  const [number1, number2] = [getRandomInt(50), getRandomInt(50)]
 
-  showQuestion(`${number1} ${number2}`);
-  const inputStr = Number(setUserInput());
-  const inputInt = Number(inputStr);
-  const leastCommonDivisor = gcd(number1, number2);
+  showQuestion(`${number1} ${number2}`)
+  const inputStr = Number(setUserInput())
+  const inputInt = Number(inputStr)
+  const leastCommonDivisor = gcd(number1, number2)
 
   if (!isInt(inputInt) || inputInt !== leastCommonDivisor) {
-    resultGame = false;
+    resultGame = false
   }
 
-  return [leastCommonDivisor, inputStr, resultGame];
-};
+  return [leastCommonDivisor, inputStr, resultGame]
+}
