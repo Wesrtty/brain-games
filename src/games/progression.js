39867a598cc00d021008331d479d09fdc99af7c8
BuildTools <n.novikov@cargoonline.ru>
2020-12-17 23:59:47 +0300

    fix errors

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,38 +1,38 @@
-import { showQuestion, setUserInput } from '../../utils/interaction.js'
-import { getRandomInt, isInt } from '../../utils/utils.js'
+import { showQuestion, setUserInput } from '../../utils/interaction.js';
+import { getRandomInt, isInt } from '../../utils/utils.js';
 
-export const getRules = () => 'What number is missing in the progression?'
+export const getRules = () => 'What number is missing in the progression?';
 
 const generateNumbers = (maxSize = 5) => {
-  const numbers = []
-  const minSize = 5
-  let sumNumbers = 0
-  const diffBetweenNumbers = getRandomInt(10)
+  const numbers = [];
+  const minSize = 5;
+  let sumNumbers = 0;
+  const diffBetweenNumbers = getRandomInt(10);
 
   for (let index = 0; index < (minSize + maxSize); index += 1) {
-    sumNumbers += diffBetweenNumbers
-    numbers.push(sumNumbers)
+    sumNumbers += diffBetweenNumbers;
+    numbers.push(sumNumbers);
   }
-  return numbers
-}
+  return numbers;
+};
 
-const hideRandomNumber = (numbers) => getRandomInt(numbers.length - 1)
+const hideRandomNumber = (numbers) => getRandomInt(numbers.length - 1);
 
 export const run = () => {
-  let resultGame = true
-  const numbers = generateNumbers(7)
+  let resultGame = true;
+  const numbers = generateNumbers(7);
 
-  const indexHiddenNumber = hideRandomNumber(numbers)
-  const hiddenNumber = numbers[indexHiddenNumber]
-  const numbersWithHiddenNumber = numbers
-  numbersWithHiddenNumber[indexHiddenNumber] = '..'
+  const indexHiddenNumber = hideRandomNumber(numbers);
+  const hiddenNumber = numbers[indexHiddenNumber];
+  const numbersWithHiddenNumber = numbers;
+  numbersWithHiddenNumber[indexHiddenNumber] = '..';
 
-  showQuestion(numbersWithHiddenNumber.join(' '))
-  const inputStr = setUserInput()
-  const inputInt = Number(inputStr)
+  showQuestion(numbersWithHiddenNumber.join(' '));
+  const inputStr = setUserInput();
+  const inputInt = Number(inputStr);
 
   if (!isInt(inputInt) || inputInt !== hiddenNumber) {
-    resultGame = false
+    resultGame = false;
   }
-  return [hiddenNumber, inputStr, resultGame]
-}
+  return [hiddenNumber, inputStr, resultGame];
+};
