244188574911cde0f98dc3ed01bbfe4c19fe79cc
BuildTools <n.novikov@cargoonline.ru>
2020-12-17 22:56:32 +0300

    fix errors

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${username}`);
+  console.log(`Hello, ${username}!`);
 
   return username;
 };
