945c2539f4b63bf08d039ed6153cd620d4103d1d
BuildTools <n.novikov@cargoonline.ru>
2020-12-17 23:56:21 +0300

    fix errors

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,28 +1,28 @@
-import { showQuestion, setUserInput } from '../../utils/interaction.js';
-import { getRandomInt } from '../../utils/utils.js';
+import { showQuestion, setUserInput } from '../../utils/interaction.js'
+import { getRandomInt } from '../../utils/utils.js'
 
-export const getRules = () => 'Answer "yes" if given number is prime. Otherwise answer "no".';
+export const getRules = () => 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
-const isValid = (input) => ['yes', 'no'].includes(input);
+const isValid = (input) => ['yes', 'no'].includes(input)
 
 const isPrime = (number) => {
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 
 export const run = () => {
-  let resultGame = true;
-  const number = getRandomInt(50);
+  let resultGame = true
+  const number = getRandomInt(50)
 
-  showQuestion(number);
-  const input = setUserInput();
+  showQuestion(number)
+  const input = setUserInput()
 
   if (!isValid(input) || !(isPrime(number) === (input === 'yes'))) {
-    resultGame = false;
+    resultGame = false
   }
-  return [input === 'yes' ? 'no' : 'yes', input, resultGame];
-};
+  return [input === 'yes' ? 'no' : 'yes', input, resultGame]
+}
