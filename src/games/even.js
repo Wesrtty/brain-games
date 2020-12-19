f4528d4a831a28aa8d2968f5ecf98548104c922a
BuildTools <n.novikov@cargoonline.ru>
2020-12-19 17:56:11 +0300

    fix errors

--- src/games/even.js
+++ src/games/even.js
@@ -1,22 +1,22 @@
-import { showQuestion, setUserInput } from '../../utils/interaction.js';
-import { getRandomInt } from '../../utils/utils.js';
+import { showQuestion, setUserInput } from '../../utils/interaction.js'
+import { getRandomInt } from '../../utils/utils.js'
 
-export const getRules = () => 'Answer "yes" if the number is even, otherwise answer "no".';
+export const getRules = () => 'Answer "yes" if the number is even, otherwise answer "no".'
 
-const isValid = (input) => ['yes', 'no'].includes(input);
+const isValid = (input) => ['yes', 'no'].includes(input)
 
-const isEven = (number) => number % 2 === 0;
+const isEven = (number) => number % 2 === 0
 
 export const run = () => {
-  let resultGame = true;
-  const number = getRandomInt(100);
+  let resultGame = true
+  const number = getRandomInt(100)
 
-  showQuestion(number);
-  const input = setUserInput();
+  showQuestion(number)
+  const input = setUserInput()
 
   if (!isValid(input) || !isEven(number) === (input === 'yes')) {
-    resultGame = false;
+    resultGame = false
   }
 
-  return [isEven(number) ? 'yes' : 'no', input, resultGame];
-};
+  return [isEven(number) ? 'yes' : 'no', input, resultGame]
+}
