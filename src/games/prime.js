64c47b5992992adfa3e3a246d9b78851c441f695
BuildTools <n.novikov@cargoonline.ru>
2020-12-19 13:20:52 +0300

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -8,8 +8,8 @@ const isValid = (input) => ['yes', 'no'].includes(input)
 const isPrime = (number) => {
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) return false;
-    return number > 1;
   }
+  return number > 1;
 }
 
 export const run = () => {
