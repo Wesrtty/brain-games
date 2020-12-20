6fc114bca38117d869e81356fcbb28018842a2a1
BuildTools <n.novikov@cargoonline.ru>
2020-12-21 00:10:12 +0300

    fix errors

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,18 +1,18 @@
-import { getRandomInt, isInt } from '../../utils/utils.js'
+import { getRandomInt, isInt } from '../../utils/utils.js';
 
-export const showRules = () => console.log('Find the greatest common divisor of given numbers.')
+export const showRules = () => console.log('Find the greatest common divisor of given numbers.');
 
 const getLeastCommonDivisor = (num1, num2) => {
   if (num2) {
-    return getLeastCommonDivisor(num2, num1 % num2)
+    return getLeastCommonDivisor(num2, num1 % num2);
   }
-  return Math.abs(num1)
-}
+  return Math.abs(num1);
+};
 
-export const getTask = () => [getRandomInt(50), getRandomInt(50)]
+export const getTask = () => [getRandomInt(50), getRandomInt(50)];
 
-export const getRightAnswer = (task) => getLeastCommonDivisor(...task)
+export const getRightAnswer = (task) => getLeastCommonDivisor(...task);
 
-export const isValid = (input) => isInt(Number(input))
+export const isValid = (input) => isInt(Number(input));
 
-export const isVictory = (task, input) => getLeastCommonDivisor(...task) === Number(input)
+export const isVictory = (task, input) => getLeastCommonDivisor(...task) === Number(input);
